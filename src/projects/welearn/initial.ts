cb1342c1f8a73b579dd0c8615e19ad17906cc80f
fashiye <142155117+fashiye@users.noreply.github.com>
2024-12-16 12:42:36 +0800

    Update initial.ts

--- src/projects/welearn/initial.ts
+++ src/projects/welearn/initial.ts
@@ -1,8 +1,13 @@
 // 在此处注册(直接调用)各个插件中，app初始化时要执行的函数
-import "./exam/initial";
-import "./exercise/initial";
-import "./time/initial";
+import { initialExam } from "./exam/initial";
+import { initialExercise } from "./exercise/initial";
+import { initialTime } from "./time/initial";
 
 import { WELearnAPI } from "@/src/api/welearn";
+export async function initialWELearn() {
+    WELearnAPI.checkVersion();
+    initialExam();
+    initialExercise();
+    initialTime();
+}
 
-WELearnAPI.checkVersion();
